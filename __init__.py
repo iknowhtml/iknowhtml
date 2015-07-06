29c0efdcd4a4755f378e58b1751e2e34eca19ec3
Aki <xag9bb@virginia.edu>
2015-07-05 23:55:21 -0400

    new background and updated content for homepage

--- __init__.py
+++ __init__.py
@@ -9,6 +9,6 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template('noob.html')
 if __name__ == "__main__":
     app.run()
