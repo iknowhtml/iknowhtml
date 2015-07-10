db5a9cd88a9ed7af20741825d3ceb71a39acc39c
Aki <xag9bb@virginia.edu>
2015-07-10 00:24:03 -0400

    gag page for tiffany

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('noob.html')
+    return render_template('what_the.html')
+
 if __name__ == "__main__":
     app.run()
