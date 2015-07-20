0a3581a72c8075ebec6df951792a6f35d8581410
Aki <xag9bb@virginia.edu>
2015-07-19 21:03:18 -0400

    first interation of deployable site

--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,9 @@
 var app = angular.module("siteApp", []);
 
 app.controller('navCtrl', ['$scope', function($scope){
+	$scope.showMobileMenu = false;
+	$scope.toggleMobileMenu = function(){
+		console.log($scope.showMobileMenu)
+		$scope.showMobileMenu = !$scope.showMobileMenu;
+	}
 }]);
