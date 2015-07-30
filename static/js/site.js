723b8d5e990d57b5d6f4a71194548c98b92339e4
iknowhtml <xag9bb@virginia.edu>
2015-07-30 00:23:43 -0400

    added resume to static resources

--- static/js/site.js
+++ static/js/site.js
@@ -1,3 +1,4 @@
+/*global $ */
 var app = angular.module("siteApp", ['parallax']);
 
 app.controller('navCtrl', ['$scope', function($scope){
@@ -5,5 +6,29 @@ app.controller('navCtrl', ['$scope', function($scope){
 	$scope.toggleMobileMenu = function(){
 		console.log($scope.showMobileMenu)
 		$scope.showMobileMenu = !$scope.showMobileMenu;
+	}
+}]);
+
+app.controller('introCtrl', ['$scope', function($scope){
+	$(document).ready(function(){
+		resizeDiv();
+	});
+
+	window.onresize = function(event) {
+		resizeDiv();
+	}
+
+	function resizeDiv() {
+		var win_height = $(window).height() < 960 ? $(window).height() : 960;	
+		var height = 0.85 * win_height;
+		var top = 0.00027 * Math.pow(height,2);
+		var size = 0.15 * win_height;
+
+		$('#intro').css('height', height+ 'px');
+
+		$('.introduction-header').css('top', top + 'px');	
+
+		$('.introduction-header-main').css('font-size', size + 'px');
+
 	}
 }]);
